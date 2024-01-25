4474d897ca4b286bc11b32920d47c58bb7f81f2d
Hrodriguez2019188 <informatica@alumno.local>
2024-01-25 11:53:49 -0600

    se agregaron imagenes ys e termino de editar el index

---
+++ scripts/logiIndex.js
@@ -0,0 +1,11 @@
+// Cambiar el color del texto de los enlaces de navegación al pasar el ratón por encima
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('mouseover', function() {
+        this.style.color = '#ff6600'; // Cambiar el color del texto al naranja al pasar el ratón
+    });
+
+    link.addEventListener('mouseout', function() {
+        this.style.color = '#fff'; // Restaurar el color original del texto al salir el ratón
+    });
+});
+
